50f5b7dcb007735c67beb6bd96790e2e0d3ed58c
Ikbal <ikbal.mwlna2350@gmail.com>
2026-05-13 08:19:37 +0700

    feat: redirect home to dashboard if authenticated

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    if (auth()->check()) {
+        return redirect()->route('dashboard');
+    }
     return view('welcome');
 });
 
