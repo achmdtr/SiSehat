5b28b39715857478f8b2dd9c105973dd31737d43
rizky <muhammadrizkyramadhan705@gmail.com>
2026-05-14 23:09:05 +0700

    menghapus arrow di halaman Manajemen Data UMKM & Karyawan

--- resources/views/manajemen-umkm.blade.php
+++ resources/views/manajemen-umkm.blade.php
@@ -239,11 +239,6 @@
                 </tbody>
             </table>
 
-            <div class="expand-btn">
-                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                    <polyline points="6 9 12 15 18 9"></polyline>
-                </svg>
-            </div>
         </div>
 
     </div>
